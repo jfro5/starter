b27ca97fc357f391946a9768cb6ff0c2d11fac7f
jfro5 <jonathan.frost@bskyb.com>
2013-12-06 15:05:55 +0000

    Added while loop and built exit functionality

--- app.rb
+++ app.rb
@@ -1,45 +1,51 @@
-sites = {
-    amazon: 'monkey1',
-    facebook: 'giraffe2'
-}
+x = 0
+while x < 1 do
 
-puts 'Choose an action'
-choice = gets.chomp.downcase
+  sites = {
+      amazon: 'monkey1',
+      facebook: 'giraffe2'
+  }
 
-case choice
-  when 'add'
-    puts 'Name of the site?'
-    title = gets.chomp.downcase
-    if sites[title.to_sym].nil?
-      puts 'What is the password?'
-      password = gets.chomp.downcase
-      sites[title.to_sym] = password
-      puts 'Thanks, your password has been stored.'
+  puts 'Choose an action'
+  choice = gets.chomp.downcase
+
+  case choice
+    when 'add'
+      puts 'Name of the site?'
+      title = gets.chomp.downcase
+      if sites[title.to_sym].nil?
+        puts 'What is the password?'
+        password = gets.chomp.downcase
+        sites[title.to_sym] = password
+        puts 'Thanks, your password has been stored.'
+      else
+        puts 'That site already has a password stored.'
+      end
+    when 'update'
+      puts 'Name of the site you want to update the password for?'
+      title = gets.chomp.downcase
+      if sites[title.to_sym].nil?
+        puts 'That site does not have a password stored.'
+      else
+        puts 'New password?'
+        password = gets.chomp.downcase
+        sites[title.to_sym] = password
+      end
+    when 'display'
+      sites.each { |title, password| puts "#{title}: #{password}" }
+    when 'delete'
+      puts 'Name of the site you want to delete?'
+      title = gets.chomp.downcase
+      if sites[title.to_sym].nil?
+        puts 'No password stored for that site.'
+      else
+        sites.delete(title)
+        puts 'Password deleted'
+      end
+    when 'exit'
+      puts 'Goodbye'
+      x = 1
     else
-      puts 'That site already has a password stored.'
-    end
-  when 'update'
-    puts 'Name of the site you want to update the password for?'
-    title = gets.chomp.downcase
-    if sites[title.to_sym].nil?
-      puts 'That site does not have a password stored.'
-    else
-      puts 'New password?'
-      password = gets.chomp.downcase
-      sites[title.to_sym] = password
-    end
-  when 'display'
-    sites.each { |title,password| puts "#{title}: #{password}"}
-  when 'delete'
-    puts 'Name of the site you want to delete?'
-    title = gets.chomp.downcase
-    if sites[title.to_sym].nil?
-      puts 'No password stored for that site.'
-    else
-      sites.delete(title)
-      puts 'Password deleted'
-    end
-  else
-    puts 'Error!'
+      puts 'Error!'
+  end
 end
-
